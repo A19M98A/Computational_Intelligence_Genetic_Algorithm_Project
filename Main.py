3a4a2cea88a70a9425017e82854ee6df5d695372
AminAllah <janipooraminallah@gmail.com>
2020-03-06 15:44:49 +0330

    Defined heder of Fitness function and Menu Bar

---
+++ Main.py
@@ -0,0 +1,11 @@
+#
+def Fitness(Chromosome):
+    pass
+
+while 1:
+    print('Parent selection [0]')
+    print('Crossover [1]')
+    print('Survivor selection [2]')
+    print('(1/0).(1/0).(1/0) ?')
+    Ans = input()
+     
