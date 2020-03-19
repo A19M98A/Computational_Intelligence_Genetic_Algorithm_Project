72efd18c9e5e10a6a7605963420271928086cd14
AminAllah <janipooraminallah@gmail.com>
2020-03-19 18:58:53 +0330

    just add som method, and edit readme

--- Main.py
+++ Main.py
@@ -7,6 +7,15 @@ def Fitness(x, y):
     ret = -20 * math.exp(-0.2 * pow((0.5 * (x * x + y * y)), 0.5)) - math.exp(0.5 * (math.cos(2 * x * math.pi) + math.cos(2 * y * math.pi))) + 2.7 + 20
     return ret
 
+def ParentSelection(x, y):
+    pass
+
+def Crossover(x, y):
+    pass
+
+def SurvivorSelection(x, y):
+    pass
+
 while 1:
     print('Parent selection [0]')
     print('Crossover [1]')
--- README.md
+++ README.md
@@ -6,3 +6,5 @@ Chromosome include 12 bit (6 bit for x, 6 bit for y)
 
 - [x] Defined a Fitness function
 - [ ] Defined a Chromosome (Binary)
+- [ ] Fill the method of Parent Selection, Crossover, Survivor Selection
+- [ ] Add Graphic Mode
